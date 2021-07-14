30e93114a4fe3e51e8e5800140777b41d43c0b80
Hamd Zulfiqar <hamd.zulfiqar@tintash.com>
2021-07-14 18:15:05 +0500

    Hit a public API from node
    WHAT: Used Axios for api hit
    
    WHY: for practice purpose
    
    IMPACT: MEDIUM

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const dotenv = require('dotenv');
 const http = require('http');
+const axios = require('axios');
 
 dotenv.config();
 
-const server = http.createServer((req,res) => {
-    if(req.url === '/first'){
-        res.write("First response from Node Backend!");
-        res.statusCode = 200;
-        res.end();
+const getTodo = async () => {
+    try{
+        var data = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+        console.log("API hit successful!!!\n");
+        return data;
     }
-    else{
-        res.write("wrong API called!");
-        res.statusCode = 404;
-        res.end();
+    catch(e){
+        console.log("API hit FAILED!!!\n");
+    }
+}
+
+const server = http.createServer(async (req,res) => {
+    //I should try to switch cases for multiple url paths
+    var url = req.url;
+
+    switch(url){
+        case '/first':{
+            res.statusCode = 200;
+            res.write("First API response from Node :D");
+            res.end();
+            break;
+        }
+        case '/second':{
+            //Here let me try to call a third party api
+            //Options I have: node-fetch or Axios
+            res.statusCode = 200;
+            var temp = await getTodo();
+
+            console.log(temp.data);
+
+            res.write(JSON.stringify(temp.data));
+            res.end();
+        }
     }
 });
 
